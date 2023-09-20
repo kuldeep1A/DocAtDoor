e93d5cab2e7b456e932bdfe8133ff5ca779cedf8
Triggered41 <vehdathamid@gmail.com>
2023-09-21 01:31:31 +0530

    Merge try

---
+++ docatdoor/build/src/App.jsx
@@ -0,0 +1,50 @@
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Error from "./pages/Error"
+import Contact from "./pages/Contact"
+import Doctor from "./pages/Doctor"
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "doctor",
+    element: <Doctor/>
+  },
+  {
+    path: "contact",
+    element: <Contact/>
+  },
+  {
+    path: "*",
+    element: <Error />
+  }
+]);
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="">
+          <header className="App-header">
+              <RouterProvider  router={router} />
+          </header>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
