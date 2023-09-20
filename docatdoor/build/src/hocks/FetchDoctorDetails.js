694db3e9760f102637d72c78b719cbc0c8d37e7a
Triggered41 <vehdathamid@gmail.com>
2023-09-21 01:20:10 +0530

    Correctly Fetched doctor details

--- docatdoor/build/src/hocks/FetchDoctorDetails.js
+++ docatdoor/build/src/hocks/FetchDoctorDetails.js
@@ -6,6 +6,7 @@ const Doctors = collection(database, "Doctors");
 
 var Filed = [{}]
 function setFiled(data) {
+    console.log("data: ", data);
     Filed = data;
 }
 
@@ -16,11 +17,15 @@ export const FetchDoctorDetials = (DoctorEmail) => {
 
     // // eslint-disable-next-line react-hooks/exhaustive-deps
     const getDetails = () => {
-        if (DoctorEmail){
-            const _query = query(Doctors, where("doctorEmail", "==", DoctorEmail))
+        if (DoctorEmail){where
+            const _query = query(Doctors)
             onSnapshot(_query, (response) => {
-                console.log(response);
+                setFiled(response.docs.map((item)=>{
+                    console.log("response: ", item.data(), item.id);
+                    return {...item.data(), id: item.id}
+                }))
             })
+
         }
     }
 
