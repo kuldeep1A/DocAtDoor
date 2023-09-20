254bdb1f7bb69d6ffa0f452b1dbba7cae4297c0a
unknown <vehdathamid@gmail.com>
2023-09-20 13:30:25 +0530

    Started doctor filtering logic

---
+++ docatdoor/src/Components/Filter.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Filter() {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+
+
+// var db = {}
+// var doctor_list = []
+// import React from "react"
+
+// const test = () => {
+//     return (
+//         <h1>
+//             Hello
+//         </h1>
+//     )
+// }
+
+// export function Filter(params) {
+//     for (let i = 0; i < Object.keys(db); i++) {
+//         const doctor = db[i];
+//         if (doctor["Specializaation"] == params["Category"]){
+//             doctor_list.push(doctor);
+//         }
+//     }
+// }
+
+// module.exports = doctor_list;
--- docatdoor/src/index.js
+++ docatdoor/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Filter from './Components/Filter';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// console.log(test);
 root.render(
   <React.StrictMode>
     <App />
+    <Filter/>
+
+    {/* <test/> */}
+    {/* console.log(); */}
   </React.StrictMode>
 );
 
