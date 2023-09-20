e3bb44459caf36ba099c34dab39a85cced0ad7ed
Jennydevid <jyotideveda02@gmail.com>
2023-09-20 15:54:22 +0530

    home navigation bar

--- docatdoor/src/App.js
+++ docatdoor/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './Components/Navbar';
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Start DocAtDoor</p>
+        {/* <p>Start DocAtDoor</p> */}
+        <Navbar/>
+
+        <Routes>
+          <Route  
+            path='/'
+            element={<Home/>}
+          />
+        </Routes>
+        
       </header>
     </div>
   );
---
+++ docatdoor/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='w-full h-[500]'>
+      {/* first section  */}
+      <div className='bg-gradient-to-l from-sky-200 via-sky-100 to-transparent h-[500px]'>
+          section 1
+      </div>
+    </div>
+  )
+}
