a9d2b0f2893b05b4e1b2117790d3a0980d6fd59b
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2023-09-21 22:14:50 +0530

    update footer with privouse version of footer and deploy

--- docatdoor/build/src/Components/Footor.jsx
+++ docatdoor/build/src/Components/Footor.jsx
@@ -9,7 +9,17 @@ export const Footer = () => {
         "Allergies",
         "Strep throat",
         "Depression"
-    ]
+    ];
+    const treatments = [
+        "Antibiotics, rest, and hydration",
+        "Antibiotics, often taken for several months",
+        "Bronchodilators, inhaled corticosteroids, and lifestyle management",
+        "Antimalarial drugs, rest, and hydration",
+        "Antihistamines, allergen avoidance, and immunotherapy",
+        "Antibiotics and rest",
+        "Psychotherapy, medication, and lifestyle changes"
+    ];
+    
   return (
     <div className='w-full flex flex-row justify-around items-start min-h-[250px] bg-gradient-to-r from-sky-600 via-sky-700 to-sky-800'>
         <div className='flex items-center text-sky-200'>
@@ -23,24 +33,32 @@ export const Footer = () => {
 
         <div className='flex flex-col text-sky-300'>
             <h2 className='text-2xl text-sky-300'>Diseases</h2>
-            <ul>
-                {
-                    diseases.map((disease, index) => (
-                        <li key={index}>
-                            {disease}
-                        </li>
-                    ))
-                }
-            </ul>
+            <div className='mt-3'>
+                <ul>
+                    {
+                        diseases.map((disease, index) => (
+                            <li key={index}>
+                                <a href={`/${disease}`}>{disease}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
 
-        <div>
-            <h3>Treatments</h3>
-            <ul>
-                {
-
-                }
-            </ul>
+        <div className='flex flex-col text-sky-300'>
+            <h3 className='text-2xl text-sky-300'>Treatments</h3>
+            <div className='mt-3'>
+                <ul>
+                    {
+                        treatments.map((treatment, index) => (
+                            <li key={index}>
+                                <a href={`/${treatment.split(" ")[0].split(",")[0]}`}>{treatment}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
     </div>
   )
